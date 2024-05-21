47570f5e455e91daa96bb805502cc6391a79b33d
mclara1marinho <maria.m.silva@edu.unipar.br>
2024-05-21 20:17:06 -0300

    start

--- app/Http/Controllers/SubtaskController.php
+++ app/Http/Controllers/SubtaskController.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
-
+use App\Http\Controllers\Controller;
 use App\Models\Subtask;
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class SubtaskController extends Controller
 {
@@ -16,30 +16,57 @@ class SubtaskController extends Controller
 
     public function store(Request $request, $taskId)
     {
-        $subtaskData = $request->all();
-        $subtaskData['id_task'] = $taskId;
-        $subtask = Subtask::create($subtaskData);
+        $rules = [
+            'name' => 'required|string|max:255',
+            'id_task' => 'required|integer'
+        ];
+
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+
+        $requestData = $request->all();
+        $requestData['id_task'] = $taskId;
+        $subtask = Subtask::create($requestData);
+
         return response()->json($subtask, 201);
     }
 
-    public function show($taskId, $subtaskId)
+    public function show($subtaskId)
     {
         $subtask = Subtask::findOrFail($subtaskId);
         return response()->json($subtask);
     }
 
-    public function update(Request $request, $taskId, $subtaskId)
+    public function update(Request $request, $subtaskId)
     {
         $subtask = Subtask::findOrFail($subtaskId);
+
+
+        $rules = [
+            'name' => 'string|max:255',
+            'id_task' => 'integer'
+        ];
+
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
         $subtask->update($request->all());
         return response()->json($subtask);
     }
 
-    public function destroy($taskId, $subtaskId)
+    public function destroy($subtaskId)
     {
         $subtask = Subtask::findOrFail($subtaskId);
         $subtask->delete();
         return response()->json(null, 204);
     }
 }
-
--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,23 @@
-<?php
+ <?php
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\SubtaskController;
 
-Route::get('/tasks', [TaskController::class, 'index']); 
-Route::post('/tasks', [taskController::class, 'store']); 
-Route::get('/tasks/{task}', [taskController::class, 'show']); 
-Route::put('/tasks/{task}', [TaskController::class, 'update']); 
-Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
+    Route::prefix('tasks')->group((function () {
+        Route::get('/tasks/subtasks', [TaskController::class, 'index']);
+        Route::post('/tasks/subtasks', [taskController::class, 'store']);
+        Route::get('/tasks/{task}/subtasks', [taskController::class, 'show']);
+        Route::put('/tasks/{task}/subtasks/{subtask}', [TaskController::class, 'update']);
+        Route::delete('/tasks/{task}/subtasks/{subtask}', [TaskController::class, 'destroy']);
+    }));
 
-Route::get('/tasks/{task}/subtasks', [SubtaskController::class, 'index']);
-Route::post('/tasks/{task}/subtasks', [SubtaskController::class, 'store']); 
-Route::get('/subtasks/{subtask}', [SubtaskController::class, 'show']); 
-Route::put('/subtasks/{subtask}', [SubtaskController::class, 'update']); 
-Route::delete('/subtasks/{subtask}', [SubtaskController::class, 'destroy']); 
+    Route::prefix('subtasks')->group((function(){
+        Route::get('/tasks/{task}/subtasks', [SubtaskController::class, 'index']);
+        Route::post('/tasks/{task}/subtasks', [SubtaskController::class, 'store']); 
+        Route::get('/subtasks/{subtask}', [SubtaskController::class, 'show']); 
+        Route::put('/subtasks/{subtask}', [SubtaskController::class, 'update']); 
+        Route::delete('/subtasks/{subtask}', [SubtaskController::class, 'destroy']); 
+    }));
+
+
